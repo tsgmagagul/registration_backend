786dea260077a4e53398556d136804d9166d73f7
Thandolwenkosi Magagula <106537445+tsgmagagul@users.noreply.github.com>
2022-08-26 08:42:55 +0200

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const userController = require('../controller/user');
-
-router
-    .route("/")
-    .post(userController.createUser)
-
-module.exports = router;
